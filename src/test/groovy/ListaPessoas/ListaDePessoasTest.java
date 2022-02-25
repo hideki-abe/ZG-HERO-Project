b1ea4757f1598a08bcf4e873ddee5b7f3d178ff2
hideki-abe <hideki-abe@hotmail.com>
2022-02-25 12:23:18 -0300

    implementação básica de testes

---
+++ src/test/groovy/ListaPessoas/ListaDePessoasTest.java
@@ -0,0 +1,26 @@
+package ListaPessoas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListaDePessoasTest {
+
+    @Test
+    void cadastraCandidato() {
+    }
+
+    @Test
+    void cadastraEmpresa() {
+    }
+
+    @Test
+    void setCandidatos() {
+    }
+
+    @Test
+    void setEmpresas() {
+    }
+
+
+}
