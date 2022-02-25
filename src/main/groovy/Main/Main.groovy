b9a6ec612d95823f6e49fd080ca4d5453bac351a
hideki-abe <hideki-abe@hotmail.com>
2022-02-25 10:59:01 -0300

    adicionando o gradle

---
+++ src/main/groovy/Main/Main.groovy
@@ -0,0 +1,147 @@
+package Main
+
+
+import Pessoas.PessoaFisica
+import Pessoas.PessoaJuridica
+
+class Main {
+/*
+  @Author Lucas Hideki Abe
+ */
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in)
+
+        ArrayList<PessoaFisica> candidatos = new ArrayList<>()
+        ArrayList<PessoaJuridica> empresas = new ArrayList<>()
+        populaCandidatos(candidatos)
+        populaEmpresas(empresas)
+
+        def x = 1
+        loop:
+        for(;;){
+            if(x != 0){
+                Random random = new Random()
+                int num = candidatos.size()
+                num = random.nextInt(num)
+                PessoaFisica candidato = candidatos.get(num)
+
+
+                println candidato
+
+                println "Dar like no candidato? [s/n]"
+
+                def like = sc.next()
+                    if(0){
+                        //codigo para curtidas aqui
+                    }
+
+                println "Digite 0 para sair do linkeTinder, ou qualquer outro número para continuar"
+                x = sc.nextInt()
+            }else {
+                println "Fim de programa"
+                break loop
+            }
+
+        }
+
+        sc.close()
+    }
+
+    static listaCandidatos(def candidatos){
+        println candidatos
+    }
+
+    static listaEmpresas(def empresas){
+        println empresas
+    }
+
+    static populaCandidatos(ArrayList candidatos){
+
+        List list = []
+
+        PessoaFisica pessoa1 = new PessoaFisica("João","joao@email.com",
+                "00000000000", 20, "Goiás", "00000000",
+                "Goiano com muita vontade de trabalhar!")
+        list = ["Java", "Spring Framework", "React"]
+        pessoa1.setCompetencias(list)
+
+
+        PessoaFisica pessoa2 = new PessoaFisica("Maria","maria@email.com",
+                "11111111111", 30, "São Paulo", "11111111",
+                "Paulista com muita vontade de trabalhar!")
+        list = ["Javascript", "React", "Angular"]
+        pessoa2.setCompetencias(list)
+
+        PessoaFisica pessoa3 = new PessoaFisica("Ricardo","ricardo@email.com",
+                "22222222222", 35, "São Paulo", "22222222",
+                "Paulista com muita vontade de trabalhar")
+        list = ["Javascript", "Html", "Css"]
+        pessoa3.setCompetencias(list)
+
+        PessoaFisica pessoa4 = new PessoaFisica("Luana","luana@email.com",
+                "33333333333", 40, "Minas Gerais", "33333333",
+                "Mineira com muita vontade de trabalhar")
+        list = ["Web Designer", "Javascript", "Html/Css", "Bootstrap"]
+        pessoa4.setCompetencias(list)
+
+        PessoaFisica pessoa5 = new PessoaFisica("Lucas","lucas@email.com",
+                "44444444444", 24, "Goiás", "74455050",
+                "Goiano com muita vontade de trabalhar")
+        list = ["Javascript", "Angular", "Html/Css"]
+        pessoa5.setCompetencias(list)
+
+        candidatos << pessoa1
+        candidatos << pessoa2
+        candidatos << pessoa3
+        candidatos << pessoa4
+        candidatos << pessoa5
+
+
+    }
+
+    static populaEmpresas(ArrayList empresas){
+
+        List list = []
+
+        PessoaJuridica empresa1 = new PessoaJuridica("Empresa 1","empresa1@email.com",
+                "00000000000", "Brasil", "Goiás", "00000000",
+                "Descrição da empresa 1!")
+        list = ["Pacote Adobe", "Cooperativismo", "Pontualidade"]
+        empresa1.setCompetencias(list)
+
+
+        PessoaJuridica empresa2 = new PessoaJuridica("Empresa2","empresa2@email.com",
+                "11111111111", "Brasil", "São Paulo", "11111111",
+                "Descrição da empresa 2!")
+        list = ["Html/Css", "Javascript", "Angular"]
+        empresa2.setCompetencias(list)
+
+        PessoaJuridica empresa3 = new PessoaJuridica("Empresa3","empresa3@email.com",
+                "22222222222", "Brasil", "São Paulo", "22222222",
+                "Descrição da empresa 3")
+        list = ["Javascript", "Html", "Css"]
+        empresa3.setCompetencias(list)
+
+        PessoaJuridica empresa4 = new PessoaJuridica("Empresa4","empresa4@email.com",
+                "33333333333", "Brasil", "Minas Gerais", "33333333",
+                "Descrição da empresa 4")
+        list = ["Web Designer", "Javascript", "Html/Css", "Bootstrap"]
+        empresa4.setCompetencias(list)
+
+        PessoaJuridica empresa5 = new PessoaJuridica("Empresa5","empresa5@email.com",
+                "44444444444", "Brasil", "Goiás", "74455050",
+                "Descrição da empresa 5")
+        list = ["Javascript", "Angular", "Html/Css"]
+        empresa5.setCompetencias(list)
+
+        empresas << empresa1
+        empresas << empresa2
+        empresas << empresa3
+        empresas << empresa4
+        empresas << empresa5
+
+
+
+    }
+
+}
---
+++ src/main/groovy/Pessoas/Pessoa.groovy
@@ -0,0 +1,7 @@
+package Pessoas
+
+interface Pessoa {
+
+
+
+}
