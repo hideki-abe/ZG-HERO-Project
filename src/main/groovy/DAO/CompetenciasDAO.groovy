5c751a827c2381fa28db3c11d032ed121b71780b
hideki-abe <hideki-abe@hotmail.com>
2022-04-11 02:25:08 -0300

    formatação da DAO de competências

--- src/main/groovy/DAO/CompetenciasDAO.groovy
+++ src/main/groovy/DAO/CompetenciasDAO.groovy
@@ -1,10 +1,8 @@
 package DAO
 
 import Competencias.Competencia
-import Pessoas.PessoaFisica
 
 import java.sql.Connection
-import java.sql.DriverManager
 import java.sql.PreparedStatement
 import java.sql.ResultSet
 import java.sql.SQLException
